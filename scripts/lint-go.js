f9af113f6e97c551ec45ddda7114deb386120f4e
José Rosendo <jose.l.rosendo.s@gmail.com>
2026-02-27 11:24:08 -0300

    Extend content of ignoredDirs

--- scripts/lint-go.js
+++ scripts/lint-go.js
@@ -2,7 +2,16 @@ const fs = require("node:fs");
 const path = require("node:path");
 const { spawnSync } = require("node:child_process");
 
-const ignoredDirs = new Set([".git", "node_modules", "data", "test-results"]);
+const ignoredDirs = new Set([
+  ".git",
+  "node_modules",
+  "data",
+  "test-results",
+  "web",
+  "docs",
+  "e2e",
+  "migrations",
+]);
 
 function collectGoFiles(rootDir) {
   const stack = [rootDir];
